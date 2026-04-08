a817c6b66541e0a9c8e91c6f8307f02ffc93fa7f
Loki <271817881+loki-cmyk@users.noreply.github.com>
2026-04-08 11:32:49 -0400

    Adjustments to stop auto-updates.

--- start.bat
+++ start.bat
@@ -1,27 +1,16 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-git pull --autostash -X ours --no-edit
+
 
 winget install -e --id Google.PlatformTools --accept-package-agreements --accept-source-agreements
 
 
-adb kill-server
-adb start-server
-
-pip install -r requirements.txt
-
 set UAT_AUTORESTART=1
-
-where python3 >nul 2>nul
-if %ERRORLEVEL% EQU 0 (
-    python3 bake_templates.py
-    python3 main.py
-    if %ERRORLEVEL% EQU 0 goto :end
-)
 
 if exist venv\Scripts\activate.bat (
     call venv\Scripts\activate.bat
+	pip install -r requirements.txt
     python bake_templates.py
     python main.py
 ) else (
@@ -29,4 +18,5 @@ if exist venv\Scripts\activate.bat (
     python main.py
 )
 
+pause
 :end
